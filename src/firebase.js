06213106d5b3e2dd879cf43a02db33a1f95ac081
unknown <anton13jeejo@gmail.com>
2020-12-27 20:38:58 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBetHFxBoL-b4OQ15dNAJtNoMSlAL-AqF0",
+  authDomain: "clone-65a08.firebaseapp.com",
+  databaseURL: "https://clone-65a08.firebaseio.com",
+  projectId: "clone-65a08",
+  storageBucket: "clone-65a08.appspot.com",
+  messagingSenderId: "52080813750",
+  appId: "1:52080813750:web:45b506cc30103a8b462817",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
