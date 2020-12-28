d0c6a218fa5a4c7058bd2422b9c22068fb479bd7
unknown <anton13jeejo@gmail.com>
2020-12-28 17:20:21 +0530

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [inputSearch, setInputSearch] = useState("");
+  return (
+    <div className="header">
+      <div className="header__left">
+        <MenuIcon />
+        <Link to="/">
+          <img
+            className="header__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/1200px-Logo_of_YouTube_%282015-2017%29.svg.png"
+            alt=""
+          />
+        </Link>
+      </div>
+      <div className="header__input">
+        <input
+          type="text"
+          placeholder="Search"
+          value={inputSearch}
+          onChange={(e) => setInputSearch(e.target.value)}
+        />
+        <Link to={`/search/${inputSearch}`}>
+          <SearchIcon className="header__inputButton" />
+        </Link>
+      </div>
+      <div className="header__icons">
+        <VideoCallIcon className="header__icon" />
+        <AppsIcon className="header__icon" />
+        <NotificationsIcon className="header__icon" />
+        <Avatar
+          alt="Anton"
+          src="https://avatars0.githubusercontent.com/u/44845055?s=400&u=da15adf6e19454410ef246b6dc9ca776492c3180&v=4"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
