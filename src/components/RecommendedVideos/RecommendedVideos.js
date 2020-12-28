1066d3363c4a5fe115e65813318b48c98b0f5088
unknown <anton13jeejo@gmail.com>
2020-12-28 17:20:42 +0530

    Initial commit

---
+++ src/components/RecommendedVideos/RecommendedVideos.js
@@ -0,0 +1,138 @@
+import React from "react";
+import VideoCard from "../VideoCard/VideoCard";
+import "./RecommendedVideos.css";
+
+const RecommendedVideos = () => {
+  return (
+    <div className="recommendedVideos">
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          title="Build a Google Weather App Clone"
+          views="10.7B Views"
+          timestamp="3 days ago"
+          channelImage="https://avatars0.githubusercontent.com/u/44845055?s=400&u=da15adf6e19454410ef246b6dc9ca776492c3180&v=4"
+          channel="Anton Francis Jeejo"
+          image="https://i.ytimg.com/vi/nhpmk2cO7Z8/maxresdefault.jpg"
+        />
+        <VideoCard
+          title="10 Life hacks"
+          views="110K Views"
+          timestamp="1 month ago"
+          channelImage="https://pbs.twimg.com/profile_images/1197302264978976768/OpsaABdE.jpg"
+          channel="Dale Keep"
+          image="https://i.ytimg.com/vi/oBHTqoR0-8M/maxresdefault.jpg"
+        />
+        <VideoCard
+          title="How to keep self closed?"
+          views="34K Views"
+          timestamp="13 days ago"
+          channelImage="https://dotorg.brightspotcdn.com/dims4/default/e12d5c3/2147483647/strip/true/crop/400x400+0+0/resize/1200x1200!/quality/90/?url=https%3A%2F%2Fdotorg.brightspotcdn.com%2Fd4%2Fe8%2F3a2cd7f54376a20b0f5a6ff60d26%2F400x400-dale-moss.jpg"
+          channel="Mike Stel"
+          image="https://pmcwwd.files.wordpress.com/2020/03/store-closed-sign.jpg"
+        />
+        <VideoCard
+          title="New York 360 view"
+          views="1.1M Views"
+          timestamp="20 days ago"
+          channelImage="https://m.media-amazon.com/images/M/MV5BNTEyMTQxYTMtM2RhNi00MjAxLThhZTYtZDRlMDU0NGE0ODUzXkEyXkFqcGdeQXVyNDE5NzQwMjY@._V1_UX214_CR0,0,214,317_AL_.jpg"
+          channel="Kenton"
+          image="https://cdn.getyourguide.com/img/location/5c04f6f20650d.jpeg/148.jpg"
+        />
+        <VideoCard
+          title="What is JWT token?"
+          views="11K Views"
+          timestamp="1 year ago"
+          channelImage="https://avatars0.githubusercontent.com/u/44845055?s=400&u=da15adf6e19454410ef246b6dc9ca776492c3180&v=4"
+          channel="Anton Francis Jeejo"
+          image="https://i.ytimg.com/vi/Xjncjdmz87Y/maxresdefault.jpg"
+        />{" "}
+        <VideoCard
+          title="House at Houston for free!!!"
+          views="110 Views"
+          timestamp="14 days ago"
+          channelImage="https://www.kirkland.com/-/media/professionals/c/cendali-dale-pc.jpg"
+          channel="Keny Saya"
+          image="https://photos.zillowstatic.com/p_e/ISvoa5k1g4jjke0000000000.jpg"
+        />{" "}
+        <VideoCard
+          title="Is it really worth $1000?"
+          views="1.7K Views"
+          timestamp="1 day ago"
+          channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTkmP9CPt9ZMCkj5viHaHqfrfhNijK5lViL-Q&usqp=CAU"
+          channel="Rijender Singh"
+          image="https://i.ytimg.com/vi/cuaAMGZtML4/maxresdefault.jpg"
+        />{" "}
+        <VideoCard
+          title="Making a flower vase with waste"
+          views="740K Views"
+          timestamp="25 days ago"
+          channelImage="https://assets.nrdc.org/sites/default/files/styles/headshot/public/dalebryk_ny_450.jpg?itok=UTQFw-e4"
+          channel="Elen Sen"
+          image="https://i.ytimg.com/vi/w65-oO3AygU/maxresdefault.jpg"
+        />{" "}
+        <VideoCard
+          title="Acapella Acoustic Version - Baby"
+          views="2.2M Views"
+          timestamp="3 years ago"
+          channelImage="https://www.cottagehealth.org/app/files/public/dale_tylor_240.jpg?size=large"
+          channel="Jeney Renes"
+          image="https://i.pinimg.com/originals/6e/06/58/6e0658f6f4d758436bd3b47028d979b2.jpg"
+        />
+        <VideoCard
+          title="How to raise seed funding on first round?"
+          views="1.1K Views"
+          timestamp="10 days ago"
+          channelImage="https://dalemyers.files.wordpress.com/2009/06/april-10-2014-21.jpg"
+          channel="Adam Ferest"
+          image="https://dashnews.org/wp-content/uploads/2019/03/What-Being-a-Nonprofit-Fundraiser-Taught-Me-About-Cryptocurrency.jpg"
+        />
+      </div>
+      <h2>Trending</h2>
+      <div className="recommendedVideos__videos">
+        <VideoCard
+          title="Naach - Hardy Sandhu| BParak | Jaani"
+          views="1.1M Views"
+          timestamp="1 day ago"
+          channelImage="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/T-series-logo.svg/220px-T-series-logo.svg.png"
+          channel="T-Series"
+          image="https://i.ytimg.com/vi/4R2Q4WjwCzU/maxresdefault.jpg"
+        />
+        <VideoCard
+          title="Jaaneman Aah - Varun Dhawan | Jacquelin | Liet"
+          views="12M Views"
+          timestamp="3 months ago"
+          channelImage="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/T-series-logo.svg/220px-T-series-logo.svg.png"
+          channel="T-Series"
+          image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQHa_cETOQlzIMn9Intn8XTGBp0Qa_7RL_8Wg&usqp=CAU"
+        />
+        <VideoCard
+          title="Hawa Hawa - Arjun Kapoor | Illena D'Cruz"
+          views="335M Views"
+          timestamp="10 days ago"
+          channelImage="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/T-series-logo.svg/220px-T-series-logo.svg.png"
+          channel="T-Series"
+          image="https://i.pinimg.com/originals/38/36/2a/38362ab843a5089a92665ae15674f85b.jpg"
+        />
+        <VideoCard
+          title="Zindagi - Tiger Shrof | Atif Aslam"
+          views="110M Views"
+          timestamp="20 days ago"
+          channelImage="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/T-series-logo.svg/220px-T-series-logo.svg.png"
+          channel="T-Series"
+          image="https://i.ytimg.com/vi/82eM7QRtoRo/maxresdefault.jpg"
+        />
+        <VideoCard
+          title="3 Peg - Nekka Kakkar | Hardy sandhu"
+          views="500M Views"
+          timestamp="15 days ago"
+          channelImage="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/T-series-logo.svg/220px-T-series-logo.svg.png"
+          channel="T-Series"
+          image="https://img.youtube.com/vi/hzTg4zPBtDU/maxresdefault.jpg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RecommendedVideos;
